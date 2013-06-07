f9e848dc070a467fd881ae5c40d5fea757b07833
Mark Schisler <mark.schisler@asynchrony.com>
2013-06-07 13:58:08 -0500

    Creating example project

---
+++ SimplifyExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimplifyExample
+//
+//  Created by Mark Schisler on 6/7/13.
+//  Copyright (c) 2013 MasterCard International, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SIMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SIMAppDelegate class]));
+    }
+}
