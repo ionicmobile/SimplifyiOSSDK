6d5004977a8ace8931eb38ed4c67e424dc1266ba
John Hainline <jihainline@gmail.com>
2013-06-18 09:20:30 -0500

    Added a done button to SIMTextField to dismiss the keyboard. This is temporary so we have a way to get to the Send button.

--- Simplify/SIMTextField.m
+++ Simplify/SIMTextField.m
@@ -3,11 +3,23 @@
 
 @implementation SIMTextField
 
+- (id)initWithFrame:(CGRect)frame {
+	if (self = [super initWithFrame:frame]) {
+		UIBarButtonItem *barButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(resignFirstResponder)];
+		UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+		toolbar.items = @[barButton];
+		self.inputAccessoryView = toolbar;
+	}
+	return self;
+}
+
 - (CGRect)textRectForBounds:(CGRect)bounds {
-    return CGRectMake(bounds.origin.x + self.textOffset.width, bounds.origin.y +  self.textOffset.height, bounds.size.width - self.textOffset.width*2, bounds.size.height -  self.textOffset.height*2);
+    return CGRectMake(bounds.origin.x + self.textOffset.width, bounds.origin.y +  self.textOffset.height,
+					  bounds.size.width - self.textOffset.width*2, bounds.size.height -  self.textOffset.height*2);
 }
 - (CGRect)editingRectForBounds:(CGRect)bounds {
-    return CGRectMake(bounds.origin.x + self.textOffset.width, bounds.origin.y +  self.textOffset.height, bounds.size.width - self.textOffset.width*2, bounds.size.height -  self.textOffset.height);
+    return CGRectMake(bounds.origin.x + self.textOffset.width, bounds.origin.y +  self.textOffset.height,
+					  bounds.size.width - self.textOffset.width*2, bounds.size.height -  self.textOffset.height);
 }
 
 @end
