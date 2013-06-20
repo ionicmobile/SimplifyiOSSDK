896d61fbb01cc4705fad8b89d32b9f704bdc4622
John Hainline <jihainline@gmail.com>
2013-06-20 10:34:55 -0500

    Changed license header for every file.

--- Simplify/SIMCreditCardEntryViewController.h
+++ Simplify/SIMCreditCardEntryViewController.h
@@ -1,3 +1,32 @@
+/* Copyright (c) 2013, Asynchrony Solutions, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *    * Neither the name of Asynchrony Solutions, Inc. nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL ASYNCHRONY SOLUTIONS, INC. BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #import <UIKit/UIKit.h>
 #import "SIMCreditCardToken.h"
 
@@ -7,9 +36,9 @@
 @protocol SIMCreditCardEntryViewControllerDelegate <NSObject>
 
 /**
- The user cancelled this attempt at credit card entry.
+ The user cancelled this attempt to generate a SIMCreditCardToken object from a credit card.
  */
-- (void)cancelled;
+- (void)tokenGenerationCancelled;
 
 /**
  The user finished sending a credit card to Simplify, and either an error or a SIMCreditCardToken was returned.
@@ -30,7 +59,7 @@
 
 /**
  Initializes a new SIMCreditCardEntryViewController. Make sure to set yourself as the delegate of this object!
- 
+
  @param publicApiToken the public API token you received when signing up to the Simplify API.
  @param showAddressView shows entry information for a US address if true.
  */
